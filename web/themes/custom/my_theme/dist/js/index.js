0bfbfe31c3319c145f0b70d2c1b55898a9e1cd03
Nazarii Lysak <nazarii.Lysak@ua.nestle.com>
2023-02-16 13:47:44 +0200

    create own theme

---
+++ web/themes/custom/my_theme/dist/js/index.js
@@ -0,0 +1,8 @@
+(function ($, Drupal) {
+    Drupal.behaviors.mainScript = {
+        attach: function (context, settings) {
+            console.log('main script')
+        }
+    };
+
+})(jQuery, Drupal);
