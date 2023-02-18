02b5a519cd547cd15033270a13e772fa5269cebe
Nazarii Lysak <nazarii.Lysak@ua.nestle.com>
2023-02-18 20:49:31 +0200

    add pages react app

--- web/themes/custom/my_theme/react_app/src/App/App.jsx
+++ web/themes/custom/my_theme/react_app/src/App/App.jsx
@@ -1,9 +1,7 @@
 import {Route, Routes} from 'react-router-dom';
 
-
 import styles from './App.module.scss';
 
-import Home from '../pages/Home/Home';
 import ReactCourse from '../pages/React_course/React_course';
 import Blog from '../pages/Blog/Blog';
 import AdditionalVideo from '../pages/Additional_video/Additional_video';
@@ -12,7 +10,6 @@ function App() {
   return (
     <div className={styles.content_wrapper}>
       <Routes>
-        <Route path={`/user/1`} element={ <Home />} />
         <Route path={`/react-course`} element={ <ReactCourse />} />
         <Route path={`blog`} element={<Blog />} />
         <Route path={`/additional-video`} element={<AdditionalVideo />}/>
